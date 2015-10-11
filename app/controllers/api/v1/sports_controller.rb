47aeeb3510ed41693b3832c9adf31d5fb5ff94a1
Gerard Morera <grdmorera@gmail.com>
2015-10-11 18:41:24 +0100

    filters/Sports.rb filters by sport. Some modification on sports_controller

--- app/controllers/api/v1/sports_controller.rb
+++ app/controllers/api/v1/sports_controller.rb
@@ -2,10 +2,15 @@ class Api
   class V1
     class SportsController < ApplicationController
       def index
-        getter = BetVictor::Content.new
-        content = getter.call
+        filter = Filters::Sports.new sports_params
+        sports = filter.call
 
-        respond_with json: :content
+        respond_with json: :sports
+      end
+
+      def sports_params
+        content = BetVictor::Content.new
+        content.call
       end
     end
   end
