25f16ca3ce6254259d0d6287244ee875272afb25
Gerard Morera <grdmorera@gmail.com>
2015-10-12 14:02:03 +0100

    minimal aesthetical commit

--- spec/requests/sports_flows_spec.rb
+++ spec/requests/sports_flows_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "SportsFlows", type: :request do
   describe "GET /sports_flows" do
-
+    
     let(:content)     { double 'content' }
     let(:get_content) do 
       "{\"version\":\"9\",\"sports\":[{\"id\":101,\"title\":\"Football\"},{\"id\":100,\"title\":\"Tenis\"}]}"
@@ -14,6 +14,7 @@ RSpec.describe "SportsFlows", type: :request do
       allow(content).to receive(:get).
         and_return(get_content)
     end
+
     it "has a 200 response status" do
       get api_v1_sports_path
       expect(response).to have_http_status(200)
