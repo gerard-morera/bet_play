0b4b082dd9861fb92e355a7afb69bf67f609bee3
Gerard Morera <grdmorera@gmail.com>
2015-10-14 15:50:38 +0200

    outcomes_controller draft

---
+++ app/controllers/api/v1/outcomes_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  module V1
+    class OutcomesController < ApplicationController
+      def index
+        getter   = Outcomes::Get.new outcomes_params, content
+        outcomes = getter.call
+
+        render json: outcomes
+      end
+
+      private
+
+      def outcomes_params
+        Params::Outcomes.new params
+      end
+
+      def content
+        content = BetVictor::Content.new
+        content.call
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   scope module: "api" do
     scope module: "v1" do
       resources :sports, only: [:index] do
-        resources :events, only: [:index] 
+        resources :events, only: [:index] do
+          resources :outcome, only: [:index]
+        end
       end
     end
   end
