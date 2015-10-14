176ad961df6dece4a2696f12842069dce6417237
Gerard Morera <grdmorera@gmail.com>
2015-10-14 18:52:56 +0200

    Outcomes::Get sketch

---
+++ app/services/outcomes/get.rb
@@ -0,0 +1,25 @@
+module Outcomes 
+  class Get
+    def initialize params, content, sport_class: Sport::Show, event_class: Event::Show
+      @params  = params
+      @content = content
+    end
+
+    def call
+    end
+
+    private
+
+    def sport
+      sport = show_sport_class.new params, content
+      sport.call
+    end
+
+    def event
+      event = show_event_class.new params, sport
+      event.call
+    end
+
+    attr_reader :params, :content
+  end
+end
---
+++ spec/params/outcomes_params_spec.rb
@@ -0,0 +1,21 @@
+describe OutcomesParams do
+  let(:raw_params) do
+    { "sport_id" => '100', "event_id" => '200' }
+  end
+
+  subject { described_class.new raw_params }
+
+  describe 'sport_id' do
+    it 'returns the value' do
+      expect(subject.sport_id).to eq('100')
+    end
+  end
+
+  describe 'event_id' do
+    it 'returns the value' do
+      expect(subject.event_id).to eq('200')
+    end
+  end
+end
+
+#missing context if not value
