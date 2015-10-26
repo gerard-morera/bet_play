31972e3632378ebe1460da9138f7eab959498af4
Gerard Morera <grdmorera@gmail.com>
2015-10-26 14:55:14 +0000

    sports displayment feature test

--- spec/features/sports_display_spec.rb
+++ spec/features/sports_display_spec.rb
@@ -2,15 +2,20 @@ require 'rails_helper'
 
 feature 'sports displayment' do
 
-  before(:all) do
+  before(:each) do
     Capybara.current_driver = :selenium
+
+    stub_request(:get, "http://www.betvictor.com/live/en/live/list.json").
+      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.1'}).
+      to_return(status: 200, body: "{\"version\":\"9\",\"sports\":[{\"id\":101,\"title\":\"Football\",\"events\":[{\"event_id\":292582210,\"title\":\"Barça-Madrid\",\"is_virtual\":\"false\"}]},{\"id\":100,\"title\":\"Tenis\"}]}", 
+        headers: {})
   end
 
-  it 'user access to the site', js: true do
+  scenario 'user access to the site' do
     visit '/'
 
-    wait_for_ajax
-    
     expect(page).to have_text("Football")
+    expect(page).to have_text("Tenis")
+    expect(page).to have_selector(".events")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,12 +6,7 @@ SimpleCov.start
 WebMock.disable_net_connect!(allow_localhost: true, allow: /http:\/\/127.0.0.1:(\d+)/)
 
 RSpec.configure do |config|
-  config.before(:each) do
-    stub_request(:get, "http://www.betvictor.com/live/en/live/list.json").
-      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.1'}).
-      to_return(status: 200, body: "{\"version\":\"9\",\"sports\":[{\"id\":101,\"title\":\"Football\",\"events\":[{\"event_id\":292582210,\"title\":\"Barça-Madrid\",\"is_virtual\":\"false\"}]},{\"id\":100,\"title\":\"Tenis\"}]}", 
-        headers: {})
-  end
+  
   config.expect_with :rspec do |expectations|
    
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
