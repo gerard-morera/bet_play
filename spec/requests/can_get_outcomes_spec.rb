2441f09a56ce534bec8a8411878cb5bd4aa860e6
Gerard Morera <grdmorera@gmail.com>
2015-10-27 15:22:56 +0000

    improving integration tests

--- spec/requests/can_get_outcomes_spec.rb
+++ spec/requests/can_get_outcomes_spec.rb
@@ -14,14 +14,33 @@ RSpec.describe "CanGetOutcomes", type: :request do
       allow(content_instance).to receive(:get).
         and_return(content)
     end
-    it "works! (now write some real specs)" do
-      get sport_event_outcomes_path(:event_id => "1728292", :sport_id => "101")
-      expect(response).to have_http_status(200)
+
+    context "with an external API working" do
+      it "has a 200 response status" do
+        get sport_event_outcomes_path(:event_id => "1728292", :sport_id => "101")
+        expect(response).to have_http_status(200)
+      end
+
+      it "renders the content" do
+        get sport_event_outcomes_path(:event_id => "1728292", :sport_id => "101")
+        expect(response.body).to eq("[{\"outcome_id\":\"111\"},{\"outcome_id\":\"222\"}]")
+      end
     end
 
-    it "works! (now write some real specs)" do
-      get sport_event_outcomes_path(:event_id => "1728292", :sport_id => "101")
-      expect(response.body).to eq("[{\"outcome_id\":\"111\"},{\"outcome_id\":\"222\"}]")
+    context "with an external API not working" do
+      let(:content) do
+        {"error_message"=>"We are having problems connecting to the server"}
+      end
+
+      it "has 200 response status" do
+        get sport_event_outcomes_path(:event_id => "1728292", :sport_id => "101")
+        expect(response).to have_http_status(200)
+      end
+
+      it "returns a message error" do
+        get sport_event_outcomes_path(:event_id => "1728292", :sport_id => "101")
+        expect(response.body).to eq("{\"error_message\":\"We are having problems connecting to the server\"}")
+      end
     end
   end
 end
