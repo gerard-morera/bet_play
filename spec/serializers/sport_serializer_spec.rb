1b41c33ca8d040800cd5d58be0324242eb8afe9c
Gerard Morera <grdmorera@gmail.com>
2015-11-01 18:50:01 +0000

    serializers tests

--- spec/serializers/sport_serializer_spec.rb
+++ spec/serializers/sport_serializer_spec.rb
@@ -1,16 +1,23 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe SportSerializer do 
-#   let(:data)  { [{}], id: id, title: title  }
-#   let(:id)    { double 'id' }
-#   let(:title) { double 'title' }
-#   let(:sport) { double 'sport', id: id, title: title }
-#   subject { described_class.new data }
+describe SportsSerializer do 
+  let(:sport_1) { double 'sport_1', :id => 1, :title => "Football" }
+  let(:sport_2) { double 'sport_2', :id => 2, :title => "Petanca" }
+  let(:sports)  { [sport_1, sport_2] }
+  
+  let :expected_json do
+    [
+      {:id => 1, :title => "Football"},
+      {:id => 2, :title => "Petanca"},
+    ]
+  end
 
-#   describe 'call' do
+  subject { described_class.new sports }
 
-#     it '' do
-#       expect(subject.call).to eq([])
-#     end
-#   end
-# end
+  describe 'call' do
+
+    it 'builds json properly' do
+      expect(subject.call).to eq expected_json
+    end
+  end
+end
